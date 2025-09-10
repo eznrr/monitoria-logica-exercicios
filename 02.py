59a8379ce0dcc962f35b9b2264280a45c7e96862
eznrr <ezngames527@gmail.com>
2025-09-10 15:20:53 -0300

    Alteracoes nos exercicios

--- 02.py
+++ 02.py
@@ -1,9 +1,9 @@
 # Exercício 2: Verificar idade
 # Complete a condição para verificar se a pessoa é maior de idade
 
-idade = int(input("Digite sua idade: "))
-
-if ... :
-    print("Maior de idade")
-else:
-    print("Menor de idade")
+"""
+    - O programa deve pedir a idade do usuário
+    - Após isso, deve verificar se o usuário é maior de idade ou não.
+Regra para ser considerado maior de idade:
+    - Ter a partir de 18 anos.
+"""
